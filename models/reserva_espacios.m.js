9bf059dbf320dc9110b192684165a4ff404681e2
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-04-07 19:35:11 -0400

    arreglo del arreglo
    Se arreglo el que se repetía el nombre en la consulta de BD

--- models/reserva_espacios.m.js
+++ models/reserva_espacios.m.js
@@ -3,7 +3,7 @@ const connection = require('./conexion');
 class reserva_espaciosModel {
     listar(){
         return new Promise((resolve, reject) => {
-            connection.query('SELECT r.id, p.nombre as solicitante, r.hora_inicio, r.hora_fin, pe.nombre as personal_solici, r.fecha, r.motivo, e.nombre as equipo_solici FROM reservas_equipos r INNER JOIN personal p ON r.solicitante = p.id INNER JOIN personal pe ON r.personal_solici = pe.id INNER JOIN equipos e ON r.equipo_solici = e.id;', function (error, results, fields) {
+            connection.query('SELECT r.id, s.nombre_apellido as solicitante_nombre, r.hora_inicio, r.hora_fin, pe.nombre as personal_solici_nombre, r.fecha, r.motivo, e.nombre as equipo_solici FROM reservas_equipos r INNER JOIN solicitantes s ON r.solicitante = s.id INNER JOIN personal pe ON r.personal_solici = pe.id INNER JOIN equipos e ON r.equipo_solici = e.id;', function (error, results, fields) {
                 if (error) throw error;
                 resolve (results);
             })

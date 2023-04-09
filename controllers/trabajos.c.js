b7d2f84a9bfef2580dbb4c0356eb46201a3c7352
Joseeee11 <118561364+SamTempest@users.noreply.github.com>
2023-04-09 00:31:33 -0400

    post bugiado en trabajos

--- controllers/trabajos.c.js
+++ controllers/trabajos.c.js
@@ -13,6 +13,25 @@ class trabajosControllers {
             })
         })
     }
+    agregar(parametro){
+        console.log(parametro);
+        return new Promise((resolve, reject) => {
+          // el if compara lo que se debe tener para agregar 
+          if (!parametro || !parametro.personal_solici || !parametro.reserva_solici || !parametro.equipos_solici || !parametro.fecha_inicio || !parametro.fecha_fin || !parametro.descripcion) {
+          reject("Se debe ingresar correctamente los parametros")
+          }
+          trabajosModel.agregar(parametro)
+          .then((resultado) =>  {
+            resolve(resultado)
+          })
+          .catch((err) => {
+            reject(err)
+          })
+        })
+    }
+    
 }
+
+
 
 module.exports= new trabajosControllers();
--- routes/trabajos.r.js
+++ routes/trabajos.r.js
@@ -16,4 +16,20 @@ router.get('/', function(req, res, next) {
     }
 )
 
+router.post('/agregar', function(req, res, next) {
+  const {id, personal_solici, reserva_solici, equipos_solici, fecha_inicio, fecha_fin, descripcion} = req.body
+  const parametro = { id,personal_solici, reserva_solici, equipos_solici, fecha_inicio, fecha_fin, descripcion}
+  parametro.id=null
+  trabajosControllers.agregar(parametro)
+  .then((resultado) => {
+    console.log("se agrego correctamente :)")
+    res.send(resultado);
+  })
+  .catch((err) => {
+    console.log("errorrrrrrr")
+    res.send(err)
+  })
+});
+
+
 module.exports = router;
